b74d3f9f3c874436601eae434c108d21f62d6b86
Filip <filipluczyk@gmail.com>
2021-09-20 19:39:44 +0200

    debug audio poits uready

--- controller.py
+++ controller.py
@@ -1,18 +1,26 @@
 import pygame, view, time, random
 from game_model import Manage
 pygame.init()
-view.window_generator()
 
+pygame.mixer.init()
+pygame.mixer.music.load("2019-11-17_-_Bold_Statement_-_David_Fesliyan.mp3")
+pygame.mixer.music.set_volume(0.7)
+pygame.mixer.music.play()
+
+view.window_generator()
 surface = pygame.Surface((250, 750))
 surface.fill((80,220,100))
-
+taudio = time.time()
 if __name__ == "__main__":
+	punkty = 0
+	tim = 1
 	while True:
 		t0 = time.time()
 		Manage.Fall()
 		
+		
 		t1 = time.time()
-		while t1 - t0 < 1:
+		while t1 - t0 < tim:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
@@ -24,7 +32,29 @@ if __name__ == "__main__":
 					elif event.key == pygame.K_UP:
 						Manage.Rotate()
 					elif event.key == pygame.K_DOWN:
-						Manage.FallFaster()
+						go_down = True
+						while go_down:
+							positionsA = positions
+							t0 = time.time()
+							Manage.FallFaster()
+							view.window_generator()
+							view.window.blit(surface, (500, 0))
+							b1 = view.button(view.window, (600, 100), "Stop")
+							b_punkty = view.button(view.window, (520, 400), "Zdobyte punkty: " + str(punkty), 23)							
+							positions = Manage.GetPositions()						
+							view.squares(0, positions, 50)
+							view.squares((0,0,0), positions, 50, 2)
+							view.line()
+							pygame.display.flip()		
+							while t1 - t0 < 0.1:
+								t1 =time.time()
+							for event in pygame.event.get():
+								if event.type == pygame.KEYUP:
+									if event.key == pygame.K_DOWN:
+										go_down = False
+							if positionsA == positions:
+								go_down = False
+								
 					elif event.key == pygame.K_x:
 						pygame.quit()
 					elif event.key == pygame.K_a:
@@ -38,7 +68,7 @@ if __name__ == "__main__":
 				elif event.type == pygame.MOUSEBUTTONDOWN:
 					if b1.collidepoint(pygame.mouse.get_pos()):
 						variable = True
-						b2 = view.button(view.window, (570, 150), "Continue")
+						b2 = view.button(view.window, (570, 150), "Kontynułuj")
 						pygame.display.flip()
 						while variable:
 							for event in pygame.event.get():
@@ -50,12 +80,27 @@ if __name__ == "__main__":
 			
 			view.window_generator()
 			view.window.blit(surface, (500, 0))
-			b1 = view.button(view.window, (600, 100), "Stop")							
+			b1 = view.button(view.window, (600, 100), "Stop")
+			b_punkty = view.button(view.window, (520, 400), "Zdobyte punkty: " + str(punkty), 23)							
 			positions = Manage.GetPositions()						
 			view.squares(0, positions, 50)
 			view.squares((0,0,0), positions, 50, 2)
 			view.line()
 			pygame.display.flip()
 			t1 = time.time()
-		
-		
+			taudio1 = time.time()
+			if int(taudio1 - taudio) % 26 == 0:
+				pygame.mixer.music.play()
+		punkty += 1
+		if punkty == 15:
+			tim = 0.8
+		elif punkty == 24:
+			tim = 0.6
+		elif punkty == 36:
+			tim = 0.5
+		elif punkty == 48:
+			tim = 0.4
+		elif punkty == 60:
+			tim = 0.3
+		elif punkty == 100:
+			tim = 0.2
--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-print(pygame.font.get_fonts())
+#print(pygame.font.get_fonts())
 (width, height) = (750, 750)
 window = pygame.display.set_mode((width, height))
 
@@ -28,63 +28,12 @@ def line(color = (0, 0, 0), xy1= (501, 0), xy2 = (501, 750), thickness = 3):
 	pygame.draw.line(window,color,xy1,xy2,thickness)
 
 
-def button(window, position, text):
-	font = pygame.font.SysFont('arial', 30)
+def button(window, position, text, height = 30):
+	font = pygame.font.SysFont('arial', height)
 	text_render = font.render(text, 1, (50, 50, 200))
 	x, y, w, h = text_render.get_rect()
 	x, y = position
 	pygame.draw.rect(window, (250, 30, 20), (x, y, w, h))
 	return window.blit(text_render, (x, y))
 	
-
-if __name__ == "__main__":
-	b1 = button(window, (600, 100), "Quit")
-	while True:
-		for event in pygame.event.get():
-			if (event.type == pygame.QUIT):
-				pygame.quit()
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				if b1.collidepoint(pygame.mouse.get_pos()):
-					pygame.quit()
-		pygame.display.flip()
-'''
-def button(window, position, text):
-    font = pygame.font.SysFont('arial', 50)
-    text_render = font.render(text, 1, (255, 0, 0))
-    x, y, w , h = text_render.get_rect()
-    x, y = position
-    pygame.draw.line(window, (150, 150, 150), (x, y), (x + w , y), 5)
-    pygame.draw.line(window, (150, 150, 150), (x, y - 2), (x, y + h), 5)
-    pygame.draw.line(window, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
-    pygame.draw.line(window, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
-    pygame.draw.rect(window, (100, 100, 100), (x, y, w , h))
-    return window.blit(text_render, (x, y))
- 
-def start():
-    print("Ok, let's go")
- 
-def menu():
-    """ This is the menu that waits you to click the s key to start """
-    b1 = button(window, (400, 300), "Quit")
-    b2 = button(window, (500, 300), "Start")
-    while True:
-        for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                key_to_start = event.key == pygame.K_s or event.key == pygame.K_RIGHT or event.key == pygame.K_UP
-                if key_to_start:
-                    start()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if b1.collidepoint(pygame.mouse.get_pos()):
-                    pygame.quit()
-                elif b2.collidepoint(pygame.mouse.get_pos()):
-                    start()
-        pygame.display.update()
-    pygame.quit()
- 
-menu()
-'''
 
