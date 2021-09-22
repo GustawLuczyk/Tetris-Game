e169d2d53b571e90d26fb4785aa94b3245fc6ebe
Filip <filipluczyk@gmail.com>
2021-09-22 18:20:34 +0200

    debug

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ from game_model import Manage
 pygame.init()
 
 pygame.mixer.init()
-pygame.mixer.music.load("2019-11-17_-_Bold_Statement_-_David_Fesliyan.mp3")
+pygame.mixer.music.load("2019-12-11_-_Retro_Platforming_-_David_Fesliyan.mp3")
 pygame.mixer.music.set_volume(0.7)
 
 
@@ -63,9 +63,9 @@ if __name__ == "__main__":
 			bonus = view.button(view.window, (50, 200), "Combo 2", height)
 			pygame.display.flip()
 		t1 = time.time()
-		t2 = time.time()'''
+		t2 = time.time()
 		while t2 - t1 < 1:
-			t2 = time.time()
+			t2 = time.time()'''
 		Manage.Fall()
 		t0 = time.time()
 		t1 = time.time()
