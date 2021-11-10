01e37ad3d4ebacdc81abaa4a7f3a9a7e5e67b5b3
Gustaw Luczyk <gustawluczyk@gmail.com>
2021-11-10 16:09:11 +0100

    Add AI interface; AI tests not working

--- AIgamer.py
+++ AIgamer.py
@@ -1,44 +1,113 @@
-#from tetris import Game
-#game = Game
+from abc import *
 
-class BestPlace:
-	'''An instance of this class finds best place
-	for current falling shape'''
+class AIprocessor:
+	'''Used by client to create logic chains'''
 
-	def __init__(self, patterns, obj_lst):
-		self.__shapes_patterns = patterns
+	def __init__(self, obj_lst, patt, position, ev_queue):
+		pass
+
+
+class Request:
+	'''Creates request objects'''
+	
+	def __init__(self, obj_lst, patt, position):
+		self.obj_lst = obj_lst
+		self.pattern = patt
+		self.position = position
+		self.possible_positions = []		
+
+
+	@property
+	def obj_lst(self):
+		return self.__obj_lst
+
+
+	@obj_lst.setter
+	def obj_lst(self, obj_lst):
 		self.__obj_lst = obj_lst
 
 
-	def FindPlace(self):
+	@property
+	def pattern(self):
+		return self.__pattern
+
+
+	@pattern.setter
+	def pattern(self, pattern):
+		self.__pattern = pattern
+
+
+	@property
+	def position(self):
+		return self.__position
+
+
+	@position.setter
+	def position(self, position):
+		self.__position = position
+
+
+	@property
+	def possible_positions(self):
+		return self.__possible_position
+
+
+	@possible_positions.setter
+	def possible_position(self, pos):
+		self.__possible_positions = pos
+
+
+
+class BaseHandler(metaclass=ABCMeta):
+	'''Abstract class declaring an interface for handlers'''
+	
+
+	@classmethod
+	def __subclasshook__(cls, subclass):
+		return (hasattr(subclass, 'Handle') and 
+				callable(subclass.Handle) or 
+				NotImplemented)
+
+
+	@abstractmethod
+	def Handle(self, path: str, file_name: str):
+		'''Handle the request'''
+		raise NotImplementedError
+
+
+
+class PosPlaceHandler(BaseHandler):
+	'''Find best place for current falling shape'''
+
+
+	def Handle(self, request):
 		'''Main method to find best place. Iterates over 
 		shape's patterns'''
 
-		good_places = []
+		possible_positions = []
+		obj_lst = request.obj_lst
 
-		for shape_patt in self.__shapes_patterns:
+		for shape_patt in request.pattern:			
 			shape_length = shape_patt[0][2]
 			low_shape = self.LowestInShape(shape_patt, shape_length)
-			good_places = self.PossiblePositions(low_shape, shape_length)
-			
-		#	x = 0
-		#	while x + shape_length < 500:
-		#		max_in_columns = self.MaxInColumns(shape_length, x)							
-		#		self.PossiblePosition(max_shape, shape_length)
-		#		x += 50
+			possible_positions = self.PossiblePositions(low_shape, shape_length, obj_lst)
+
+		return request.possible_positions.append(possible_positions)
 
 
-	def PossiblePositions(self, low_shape, shape_length):
+	def PossiblePositions(self, low_shape, shape_length, obj_lst):
 		'''Returns posible shape's positions [(x, y) ... n] excluding those that 
 		create free spaces below the shape.'''
 
 		lst = []
 		x = 0
 		while x + shape_length < 500:
-			max_in_columns = self.MaxInColumns(shape_length, x)							
+			max_in_columns = self.MaxInColumns(shape_length, x, obj_lst)							
 			a = [max_in_columns[i] - 50 - low_shape[i] for i in range(len(low_shape))]
+			print(a)
 			if max(a) == min(a):
 				lst.append((x, a[0]))
+			#	print('condition is true')
 			x += 50
 		
 		return lst
@@ -55,14 +124,14 @@ class BestPlace:
 		return lst
 
 
-	def MaxInColumns(self, shape_length, x):
+	def MaxInColumns(self, shape_length, x, obj_lst):
 		'''Finds highest brick objects in columns that are considered and returns
 		them in a list [y0, ... yn]'''
 
 		lst = []
 
 		for i in range(shape_length // 50):
-			a = [obj.y for obj in self.__obj_lst[:-4] if obj.x == x + i*50]
+			a = [obj.y for obj in obj_lst[:-4] if obj.x == x + i*50]
 			if len(a) != 0:
 				lst.append(min(a))
 			else:
--- test_AIgamer.py
+++ test_AIgamer.py
@@ -10,9 +10,19 @@ def test_LowestInShape():
 	assert lst == [0, 50, 50]
 
 
-def test_MaxInColumns():
+def test_MaxInColumns2():
 	'''Checks if the method returns correct list'''
 
 	ai = AIgamer.BestPlace([], [model.Brick(0, 400, (0,0,0)), model.Brick(0, 450, (0,0,0)), model.Brick(50, 700, (0,0,0)), model.Brick(50, 0, (0,0,0)), model.Brick(50, 0, (0,0,0)), model.Brick(50, 0, (0,0,0)), model.Brick(50, 0, (0,0,0))])
 	lst = ai.MaxInColumns(150, 0)
 	assert lst == [400, 700, 750] 
+
+
+def test_PossiblePositions():
+	'''Checks if the method returns possible positions'''
+
+	ai = AIgamer.BestPlace([[(200, -50, 150), (50, 0), (100, 0), (0, 50), (50, 50)], [(200, -100, 100), (0, 0), (0, 50), (50, 50), (50, 100)]], [model.Brick(0, 650, (0,0,0)), model.Brick(50, 700, (0,0,0)), model.Brick(0, 400, (0,0,0)), model.Brick(0, 450, (0,0,0)), model.Brick(50, 700, (0,0,0)), model.Brick(50, 0, (0,0,0))])
+	shape_length = 100
+	low_shape = ai.LowestInShape([(200, -100, 100), (0, 0), (0, 50), (50, 50), (50, 100)], shape_length)
+	lst = ai.PossiblePositions(low_shape, shape_length)
+	assert lst == [(0, 550), (50, 600)]
