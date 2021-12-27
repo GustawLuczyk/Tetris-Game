e1969c8037609136c95dcdb6a324617930db1130
Gustaw Luczyk <gustawluczyk@gmail.com>
2021-12-27 10:02:03 +0100

    Updated tests

--- AIgamer.py
+++ AIgamer.py
@@ -73,7 +73,7 @@ class Request:
 
 
 
-class BaseHandler(metaclass=ABCMeta):
+class HandlerInterface(metaclass=ABCMeta):
 	'''Abstract class declaring an interface for handlers'''
 	
 
@@ -91,28 +91,55 @@ class BaseHandler(metaclass=ABCMeta):
 
 
 
-class PosPlaceHandler(BaseHandler):
+class BaseHandler(HandlerInterface):
+	'''Base Handler for all concrete Handlers that stores references to
+	next Handlers'''
+
+	def __init__(self, type, request, next_handler):
+		self.__request = request
+		self.__next_handler = next_handler
+		self.__concrete_handler = type(request)
+
+
+	def Handle(self):
+		'''Invoke Handle method of the concrete handler; decide if to pass the 
+		request to next Handler'''
+
+		self.__concrete_handler.Handle()
+
+		positions = self.__request.possible_positions
+		if len(positions) > 1:
+			self.__next_handler.Handle()
+		elif len(positions) == 1:
+			if len(positions[0][1]) > 1:
+				self.__next_handler.Handle()
+			elif len(positions[0][1]) == 1:
+				self.__request.final_position = (positions[0][0], positions[0][1][1])
+
+
+
+class PosPlaceHandler(HandlerInterface):
 	'''Find best place for current falling shape'''
 
 
-	def __init__(self):
-		pass
+	def __init__(self, request: object):
+		self.__request = request
 
 
-	def Handle(self, request: object):
+	def Handle(self):
 		'''Main method to find best place. Iterates over 
 		shape's patterns. Returns list: [(shape_patt, [(x, y), ... n]) ... n]'''
 
 		possible_positions = []
-		obj_lst = request.obj_lst
+		obj_lst = self.__request.obj_lst
 
-		for shape_patt in request.pattern:			
+		for shape_patt in self.__request.pattern:			
 			shape_length = shape_patt[0][2]
 			low_shape = self.LowestInShape(shape_patt, shape_length)
 			positions = self.PossiblePositions(low_shape, shape_length, obj_lst)
 			pos = (shape_patt, positions)
 			possible_positions.append(pos)
-		return request.possible_positions.extend(possible_positions)
+		return self.__request.possible_positions.extend(possible_positions)
 
 
 	def PossiblePositions(self, low_shape, shape_length, obj_lst):
@@ -159,41 +186,26 @@ class PosPlaceHandler(BaseHandler):
 
 
 
-class RowsHandler(BaseHandler):
+class RowsHandler(HandlerInterface):
 	'''Find positionings that allow deleting a row; discard the rest of
 	positionings'''
 
 
-	def __init__(self):
-		pass
+	def __init__(self, request: object):
+		self.__request = request
 
 
-	def Handle(self, request: object):
+	def Handle(self):
 		'''Check the possibility of removing rows. If no possibility, possible_positions list
 		remains unchanged. Otherwise the list containing positions allowing removing rows
 		is being updated'''
 		
-		positions_removing_rows = self.FindPossibilities(request)
-		length = len(positions_removing_rows)
-
-		if length > 1:
-			request.possible_positions = positions_removing_rows
-		elif length == 1:
-			if len(positions_removing_rows[0][1]) > 1:
-				request.possible_positions = positions_removing_rows
-			elif len(positions_removing_rows[0][1]) == 1:
-				request.final_position = positions_removing_rows[0]
-
-		
-	def FindPossibilities(self, request: object):
-		'''Create list with positions, that allow removing the highest number of rows'''
-
 		max = 1
 		positions_removing_rows = []
-		for shape in request.possible_positions:
+		for shape in self.__request.possible_positions:
 			positions = []
 			for shape_pos in shape[1]:			
-				counter = self.CountRowsSupposition(request, shape[0], shape_pos)			
+				counter = self.CountRowsSupposition(self.__request, shape[0], shape_pos)			
 				if counter == max:
 					positions.append(shape_pos)
 				elif counter > max:
@@ -203,14 +215,14 @@ class RowsHandler(BaseHandler):
 			if len(positions) > 0:
 				positions_removing_rows.append((shape[0], positions))
 
-		return positions_removing_rows
+		self.__request.possible_positions = positions_removing_rows
 
 
 	def CountRowsSupposition(self, request, patt, shape_pos):
 		'''Count how many there would be objects in rows if shape was placed
 		in shape_pos position'''
 
-		rows, row_y_coordinates = self.CountObjsRows(request)
+		rows, row_y_coordinates = self.CountObjsRows()
 		rows_copy = rows.copy()
 		objs_positions = [shape_pos[1] + patt[i][1] for i in range(1, 5)]
 
@@ -223,42 +235,56 @@ class RowsHandler(BaseHandler):
 		return counter
 
 
+	def CountObjsRows(self):
+		'''Count objects in rows except for shape's objects'''
 
-class LevelHandler(BaseHandler):
+		row_y_coordinates = [700 - n * 50 for n in range(15)]
+		rows = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+		for obj in self.__request.obj_lst[:-4]:
+			for index, y_coor in enumerate(row_y_coordinates):
+				if obj.y == y_coor:
+					rows[index] += 1
+
+		return rows, row_y_coordinates
+
+
+
+class LevelHandler(HandlerInterface):
 	'''Check which position is the lowest and choose final position'''
 
 
-	def __init__(self):
-		pass
+	def __init__(self, request: object):
+		self.__request = request
 
 
-	def Handle(self, request: object):
+	def Handle(self):
 		'''Find lowest positions and choose one of them randomly - that is the
 		final position'''
 
-		min, patts = self.FindMinPatt(request)
+		min, patts = self.FindMinPatt()
 
 		patt = random.choice(patts)
 		min_positions = []
 
-		for shape in request.possible_positions:
+		for shape in self.__request.possible_positions:
 			if patt == shape[0]:
 				for pos in shape[1]:
 					if pos[1] == min:
 						min_positions.append(pos)
 
 		min_pos = random.choice(min_positions)
-		request.final_position = (patt, min_pos)
+		self.__request.final_position = (patt, min_pos)
 
 
-	def FindMinPatt(self, request: object):
+	def FindMinPatt(self):
 		'''Find lowest positions (highest y coordinate value) and shape patterns
 		in which they occur'''
 
 		min = 0
 		patts = []
 
-		for shape in request.possible_positions:
+		for shape in self.__request.possible_positions:
 			for pos in shape[1]:
 				if pos[1] == min and shape[0] not in patts:
 					patts.append(shape[0])
--- test_AIgamer.py
+++ test_AIgamer.py
@@ -2,59 +2,64 @@ import AIgamer, new_model
 model = new_model
 
 
-def test_LowestInShape():
+def test_PosPlaceHandler_LowestInShape():
 	'''Checks if the method returns correct list'''
 
-	ai = AIgamer.PosPlaceHandler()
+	request = AIgamer.Request([], [], 0)
+	ai = AIgamer.PosPlaceHandler(request)
 	lst = ai.LowestInShape([(200, -50, 150), (0, 0), (50, 0), (50, 50), (100, 50)], 150)
+	
 	assert lst == [0, 50, 50]
 
 
-def test_MaxInColumns2():
+def test_PosPlaceHandler_MaxInColumns2():
 	'''Checks if the method returns correct list.'''
 	
-	ai = AIgamer.PosPlaceHandler()
+	request = AIgamer.Request([], [], 0)
+	ai = AIgamer.PosPlaceHandler(request)
 	obj_lst = [model.Brick(0, 400, (0,0,0)), model.Brick(0, 450, (0,0,0)), model.Brick(50, 700, (0,0,0)), 
 	model.Brick(50, 0, (0,0,0)), model.Brick(50, 0, (0,0,0)), model.Brick(50, 0, (0,0,0)), model.Brick(50, 0, (0,0,0))] #last four provided in order to cover [:-4] requirenment
 	lst = ai.MaxInColumns(150, 0, obj_lst)
+	
 	assert lst == [400, 700, 750] 
 
 
-def test_PossiblePositions():
+def test_PosPlaceHandler_PossiblePositions():
 	'''Checks if the method returns possible positions'''
-
-	ai = AIgamer.PosPlaceHandler()
+	
+	request = AIgamer.Request([], [], 0)
+	ai = AIgamer.PosPlaceHandler(request)
 	obj_lst = [model.Brick(0, 650, (0,0,0)), model.Brick(50, 700, (0,0,0)), model.Brick(400, 700, (0,0,0)),
 	model.Brick(0, 0, (0,0,0)), model.Brick(0, 0, (0,0,0)), model.Brick(0, 0, (0,0,0)), model.Brick(0, 0, (0,0,0))]
 	low_shape = ai.LowestInShape([(200, -100, 100), (0, 0), (0, 50), (50, 50), (50, 100)], 100)
 	lst = ai.PossiblePositions(low_shape, 100, obj_lst)
+	
 	assert lst == [(0, 550), (50, 600), (400, 600)]
 
 
-def test_PosPos_Handle():
+def test_PosPlaceHandler_Handle():
 	'''Check if PosPlaceHandler's Handle method correctly changes the list in the request'''
 
 	obj_lst = [model.Brick(0, 650, (0,0,0)), model.Brick(50, 700, (0,0,0)), model.Brick(400, 700, (0,0,0)), 
 	model.Brick(0, 0, (0,0,0)), model.Brick(0, 0, (0,0,0)), model.Brick(0, 0, (0,0,0)), model.Brick(0, 0, (0,0,0))]
 	patt = [[(200, -150, 50), (0, 0), (0, 50), (0, 100), (0, 150)], [(200, 0, 200), (0, 0), (50, 0), (100, 0), (150, 0)]]
-	ai = AIgamer.PosPlaceHandler()
 	request = AIgamer.Request(obj_lst, patt, 0)
-	ai.Handle(request)
-	assert request.possible_positions == [(patt[0], [(0, 450), (50, 500), (100, 550), (150, 550), (200, 550), (250, 550), (300, 550), (350, 550), (400, 500), (450, 550)]),
+	ai = AIgamer.PosPlaceHandler(request)
+	ai.Handle()
+	
+	assert request.possible_positions == [(patt[0], 
+	[(0, 450), (50, 500), (100, 550), (150, 550), (200, 550), (250, 550), (300, 550), (350, 550), (400, 500), (450, 550)]),
 	(patt[1], [(100, 700), (150, 700), (200, 700)])]
 
 	obj_lst = [model.Brick(i * 50, 700, (0,0,0)) for i in range(10)]
 	patt = [[(200, -150, 50), (0, 0), (0, 50), (0, 100), (0, 150)], [(200, 0, 200), (0, 0), (50, 0), (100, 0), (150, 0)]]
-	ai = AIgamer.RowsHandler()
 	request = AIgamer.Request(obj_lst, patt, 0)
-	b = AIgamer.PosPlaceHandler()
-	b.Handle(request)
-	assert request.possible_positions == [([(200, -150, 50), (0, 0), (0, 50), (0, 100), (0, 150)], [(0, 500), (50, 500), (100, 500), (150, 500), (200, 500), (250, 500), (300, 550), (350, 550), (400, 550), (450, 550)]), ([(200, 0, 200), (0, 0), (50, 0), (100, 0), (150, 0)], [(0, 650), (50, 650), (100, 650), (300, 700)])]
+	ai = AIgamer.PosPlaceHandler(request)
+	ai.Handle()
 
-
-	# [(0, 450), (50, 500), (100, 550), (150, 550), (200, 550), (250, 550), (300, 550), (350, 550), (400, 550), (450, 550)]
-	# [(0, 500), (50, 500), (100, 550), (150, 550), (200, 550), (250, 550), (300, 550), (350, 550), (400, 550), (450, 550)]
-	# [(0, 500), (50, 500), (100, 500), (150, 500), (200, 500), (250, 500), (300, 550), (350, 550), (400, 550), (450, 550)]
+	assert request.possible_positions == [([(200, -150, 50), (0, 0), (0, 50), (0, 100), (0, 150)], 
+	[(0, 500), (50, 500), (100, 500), (150, 500), (200, 500), (250, 500), (300, 550), (350, 550), (400, 550), (450, 550)]), 
+	([(200, 0, 200), (0, 0), (50, 0), (100, 0), (150, 0)], [(0, 650), (50, 650), (100, 650), (300, 700)])]
 
 
 def test_RowsHandler_CountObjsRows():
@@ -64,10 +69,11 @@ def test_RowsHandler_CountObjsRows():
 	obj_lst = [model.Brick(50 + i * 50, 700, (0,0,0)) for i in range(9)] 
 	obj_lst.extend([model.Brick(50 + i * 50, 650, (0,0,0)) for i in range(9)])
 	obj_lst.extend([model.Brick(200 + i * 50, 600, (0,0,0)) for i in range(10)])
-	ai = AIgamer.RowsHandler()
 	request = AIgamer.Request(obj_lst, [], 0)
-	rows, y_coor = ai.CountObjsRows(request)
+	ai = AIgamer.RowsHandler(request)	
+	rows, y_coor = ai.CountObjsRows()
 	row_y_coordinates = [700 - n * 50 for n in range(15)]
+	
 	assert rows == [9, 9, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	assert y_coor == row_y_coordinates
 
@@ -79,24 +85,26 @@ def test_RowsHandler_CountRowsSupposition():
 	obj_lst.extend([model.Brick(50 + i * 50, 650, (0,0,0)) for i in range(9)])
 	obj_lst.extend([model.Brick(200 + i * 50, 600, (0,0,0)) for i in range(10)])
 	patt = [(200, -150, 50), (0, 0), (0, 50), (0, 100), (0, 150)]
-	ai = AIgamer.RowsHandler()
 	request = AIgamer.Request(obj_lst, [], 0)
+	ai = AIgamer.RowsHandler(request)
 	counter = ai.CountRowsSupposition(request, patt, (0, 550))
 	assert counter == 2
 
 
-def test_RowsHandler_FindPossibilities():
-	'''Check if the method FindPossibilities returns correctly modified list'''
+def test_RowsHandler_Handle():
+	'''Check if the method saves proper positions according to the established
+	data structure'''
 
 	obj_lst = [model.Brick(50 + i * 50, 700, (0,0,0)) for i in range(9)] 
 	obj_lst.extend([model.Brick(50 + i * 50, 650, (0,0,0)) for i in range(9)])
 	obj_lst.extend([model.Brick(200 + i * 50, 700, (0,0,0)) for i in range(10)])
 	patt = [[(200, -150, 50), (0, 0), (0, 50), (0, 100), (0, 150)], [(200, 0, 200), (0, 0), (50, 0), (100, 0), (150, 0)]]
-	ai = AIgamer.RowsHandler()
 	request = AIgamer.Request(obj_lst, patt, 0)
+	ai = AIgamer.RowsHandler(request)	
 	request.possible_positions = [(patt[0], [(50, 400), (0, 550), (450, 350)]), (patt[1], [(0, 550), (300, 500)])]
-	ret = ai.handle(request)
-	assert ret == [(patt[0], [(0, 550)])]
+	ai.Handle()
+
+	assert request.possible_positions == [(patt[0], [(0, 550)])]
 
 
 def test_LevelHandler_FindMinPatt():
@@ -104,11 +112,12 @@ def test_LevelHandler_FindMinPatt():
 	patterns that contain that position'''
 
 	patt = [[(200, -100, 100), (0, 0), (0, 50), (50, 50), (0, 100)], [(200, -50, 150), (0, 0), (50, 0), (100, 0), (50, 50)],
-	[(200, -100, 100), (50, 0), (0, 50), (50, 50), (50, 100)], [(200, -50, 150), (50, 0), (0, 50), (50, 50), (100, 50)]]
-	ai = AIgamer.LevelHandler()
+	[(200, -100, 100), (50, 0), (0, 50), (50, 50), (50, 100)], [(200, -50, 150), (50, 0), (0, 50), (50, 50), (100, 50)]]	
 	request = AIgamer.Request([], patt, 0)
+	ai = AIgamer.LevelHandler(request)
 	request.possible_positions = [(patt[0], []), (patt[1], []), (patt[2], [(0, 500), (0, 550), (50, 550)]), (patt[3], [(0, 550)])]
-	min, patts = ai.FindMinPatt(request)
+	min, patts = ai.FindMinPatt()
+
 	assert min == 550
 	assert patts == [patt[2], patt[3]]
 
@@ -119,10 +128,18 @@ def test_LevelHandler_Handle():
 
 	patt = [[(200, -100, 100), (0, 0), (0, 50), (50, 50), (0, 100)], [(200, -50, 150), (0, 0), (50, 0), (100, 0), (50, 50)],
 	[(200, -100, 100), (50, 0), (0, 50), (50, 50), (50, 100)], [(200, -50, 150), (50, 0), (0, 50), (50, 50), (100, 50)]]
-	ai = AIgamer.LevelHandler()
 	request = AIgamer.Request([], patt, 0)
+	ai = AIgamer.LevelHandler(request)
 	request.possible_positions = [(patt[0], []), (patt[1], []), (patt[2], [(0, 500), (0, 550), (50, 550)]), (patt[3], [(0, 550)])]
-	ai.Handle(request)
+	ai.Handle()
 
 	assert request.final_position[0] in [patt[2], patt[3]]
 	assert request.final_position[1] in [(0, 550), (50, 550)]
+
+
+def test_BaseHandler():
+	'''Check if the handler invokes next handler when there is more than
+	one possible position'''
+
+	next = AIgamer.BaseHandler(None, None, None)
+	base = AIgamer.BaseHandler(AIgamer.PosPlaceHandler, request, next)
